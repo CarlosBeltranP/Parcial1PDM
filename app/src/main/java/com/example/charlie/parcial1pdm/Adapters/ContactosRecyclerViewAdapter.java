686b0a009a57e9159edc3009b60922c61251bb14
CarlosBeltranP <00507511@uca.edu.sv>
2018-05-08 11:50:15 -0600

    Lista de Contactos agregada Lista de Contactos con funcionalidad de llamada agregada

--- app/src/main/java/com/example/charlie/parcial1pdm/Adapters/ContactosRecyclerViewAdapter.java
+++ app/src/main/java/com/example/charlie/parcial1pdm/Adapters/ContactosRecyclerViewAdapter.java
@@ -1,15 +1,25 @@
 package com.example.charlie.parcial1pdm.Adapters;
 
+import android.Manifest;
+import android.content.ClipData;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.charlie.parcial1pdm.Models.ModelContactos;
 import com.example.charlie.parcial1pdm.R;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -24,7 +34,6 @@ public class ContactosRecyclerViewAdapter extends RecyclerView.Adapter<Contactos
 
     public  ContactosRecyclerViewAdapter(Context context, List<ModelContactos> listaContactos){
 
-
         mListaContactos = listaContactos;
 
         mContext = context;
@@ -35,10 +44,12 @@ public class ContactosRecyclerViewAdapter extends RecyclerView.Adapter<Contactos
         View view = inflater.inflate(R.layout.eltos_contactos,parent,false);
         ViewHolder viewHolder = new ViewHolder(view);
         return viewHolder;
+
+
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(ViewHolder holder, final int position) {
 
         TextView nombre_contacto, numero_contacto;
 
@@ -49,7 +60,20 @@ public class ContactosRecyclerViewAdapter extends RecyclerView.Adapter<Contactos
         numero_contacto.setText(mListaContactos.get(position).getNumero());
 
 
+        holder.llamada.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:"+mListaContactos.get(position).getNumero()));
+
+                if(ContextCompat.checkSelfPermission(mContext, Manifest.permission.CALL_PHONE)!=
+                        PackageManager.PERMISSION_GRANTED )
+                    return;
+                mContext.startActivity(intent);
+            }
+
+        });
 
     }
 
@@ -58,9 +82,18 @@ public class ContactosRecyclerViewAdapter extends RecyclerView.Adapter<Contactos
         return mListaContactos.size();
     }
 
+    public  void filterList(List<ModelContactos> filteredList){
+        mListaContactos = filteredList;
+        notifyDataSetChanged();
+    }
+
+
+
     public class ViewHolder extends RecyclerView.ViewHolder{
 
         TextView nombre_contacto, numero_contacto;
+        Button llamada;
+        ImageButton contacto;
 
 
         public ViewHolder(View itemView) {
@@ -68,6 +101,10 @@ public class ContactosRecyclerViewAdapter extends RecyclerView.Adapter<Contactos
 
             nombre_contacto = itemView.findViewById(R.id.tv_nombre_contacto);
             numero_contacto = itemView.findViewById(R.id.tv_numero_contacto);
+
+            llamada = itemView.findViewById(R.id.boton_llamar);
+            contacto = itemView.findViewById(R.id.ibtn_contacto);
+
         }
     }
 }
--- app/src/main/java/com/example/charlie/parcial1pdm/Fragments/FragmentContacto.java
+++ app/src/main/java/com/example/charlie/parcial1pdm/Fragments/FragmentContacto.java
@@ -8,10 +8,13 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 import com.example.charlie.parcial1pdm.Adapters.ContactosRecyclerViewAdapter;
@@ -27,6 +30,7 @@ import java.util.List;
  */
 
 public class FragmentContacto extends Fragment {
+    private ContactosRecyclerViewAdapter mAdapter;
 
     Button infoContacto;
 
@@ -41,6 +45,7 @@ public class FragmentContacto extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        getContacts();
 
         v=inflater.inflate(R.layout.fragment_contacto,container,false);
 
@@ -55,24 +60,65 @@ public class FragmentContacto extends Fragment {
          ContactosRecyclerViewAdapter adapter = new ContactosRecyclerViewAdapter(getContext(),getContacts());
 
          recyclerView.setAdapter(adapter);
-//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ OJO
-        ImageButton btnOpen = (ImageButton) v.findViewById(R.id.ibtn_contacto);
+
+     /*   ImageButton btnOpen = (ImageButton) v.findViewById(R.id.ibtn_contacto);
         btnOpen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getActivity(), Main2Activity.class);
                 startActivity(intent);
             }
+        });*/
+
+        final EditText editText  =  v.findViewById(R.id.et_buscar);
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence.length() > 0 && (charSequence.toString().contains("-") || charSequence.toString().contains(" "))) {
+                    editText.setText(charSequence.toString().replace("-", "_").replace(" ", "_"));
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                filter(s.toString());
+            }
         });
+
 
 
 
         return v;
     }
+    private void filter(String text){
+
+        List<ModelContactos> filteredList = new ArrayList<>();
+
+        for (ModelContactos item : getContacts()){
+            if(item.getNombre().toLowerCase().contains(text.toLowerCase())){
+                filteredList.add(item);
+            }
+        }
+
+        mAdapter.filterList(filteredList);
+
+
+    }
+
 
     private List<ModelContactos> getContacts(){
 
-        List<ModelContactos> list = new ArrayList<>();
+
+
+      List<ModelContactos> list = new ArrayList<>();
+
+        ContactosRecyclerViewAdapter adapter;
 
         Cursor cursor = getContext().getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
                 null, null, ContactsContract.Contacts.DISPLAY_NAME + " DESC");
@@ -86,5 +132,9 @@ public class FragmentContacto extends Fragment {
         }
 
         return list;
+
+
     }
+
+
 }
